5f0bb977404ffa0527a46a6bdaba3cc48824a44c
chamannarved <chamannarved@gmail.com>
2021-02-04 20:50:06 +0530

    Redesign navbar

---
+++ includes/navbar.php
@@ -0,0 +1,16 @@
+<nav class="bg-blue-400 shadow-md">
+	<div class="flex justify-between w-11/12 m-auto items-center">
+		<div class="text-white text-2xl font-bold">
+			<h1 class="p-2">School Managment System</h1>
+		</div>
+		<div class="text-lg text-gray-900 font-medium">
+			<ul class="flex">
+				<li class="p-2"><a href="#">Home</a></li>
+				<li class="p-2"><a href="#">About</a></li>
+				<li class="p-2"><a href="#">Contect</a></li>
+				<li class="p-2"><a href="#">Login</a></li>
+				<li class="p-2"><a href="#">Sign up</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
